9cc4e480644ec83d55869fa0031276942ce04459
Flavio Rozendo <s2it_frozendo@uolinc.com>
2024-01-04 14:21:16 -0300

    Product accordion partial

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const activeClass = 'active';
 const tabs = document.querySelectorAll('.products .tabs span');
 const productContents = document.querySelectorAll('.products .content');
 
+const accordions = document.querySelectorAll('main .products .content .accordion');
+
 tabs[0].classList.add(activeClass);
 productContents[0].classList.add(activeClass);
 
@@ -35,3 +37,41 @@ function addClass($event) {
 tabs.forEach(tab => {
     tab.addEventListener('click', addClass);
 });
+
+function toggle($event) {
+    $event.stopPropagation();
+    const target = $event.currentTarget;
+    if (target) {
+        
+        const activePanel = document.querySelector(`main .products .content .panel.${activeClass}`);
+        if (activePanel) {
+            activePanel.classList.toggle('active');
+        }
+
+        const activeAccordion = document.querySelector(`main .products .content .accordion.${activeClass}`);
+        if (activeAccordion) {
+            activeAccordion.classList.toggle('active');
+        }
+
+        const activePicture = document.querySelector(`main .products .content .product-photo img.${activeClass}`);
+        if (activePicture) {
+            activePicture.classList.toggle('active');
+        }
+
+        target.classList.toggle('active');
+
+        const id = target.id;
+        const pictureId = `${id}-picture`;
+        const pictureItem = document.getElementById(pictureId);
+        pictureItem.classList.toggle('active');
+
+        const panel = target.nextElementSibling;
+        if (panel) {
+            panel.classList.toggle('active');
+        }
+    }
+}
+
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', toggle);
+})
