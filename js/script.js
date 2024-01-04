6d2e9ef91151461794430d0e466fcdcc6a44a937
Flavio Rozendo <s2it_frozendo@uolinc.com>
2024-01-04 15:19:07 -0300

    Refactor to clean the code

--- js/script.js
+++ js/script.js
@@ -1,77 +1,119 @@
 const activeClass = 'active';
-const tabs = document.querySelectorAll('.products .tabs span');
-const productContents = document.querySelectorAll('.products .content');
 
-const accordions = document.querySelectorAll('main .products .content .accordion');
-
-tabs[0].classList.add(activeClass);
-productContents[0].classList.add(activeClass);
-
-function removeActiveClass() {
-    const tabElement = document.querySelector(`.products .tabs span.${activeClass}`);
+const disableTab = () => {
+    const tabElement = document.querySelector('[data-tab-active="true"]');
     if (tabElement) {
         tabElement.classList.remove(activeClass);
+        tabElement.dataset.tabActive = false;
+        disableProductContent(tabElement.id);
     }
-    const contentElement = document.querySelector(`.products .content.${activeClass}`);
+}
+
+const disableProductContent = (id) => {
+    const contentId = `${id}-content`;
+    const contentElement = document.getElementById(contentId);
     if (contentElement) {
         contentElement.classList.remove(activeClass);
     }
 }
 
-function addClass($event) {
+const enableTab = ($event) => {
     $event.stopPropagation();
     const target = $event.currentTarget;
+
     if (target) {
         const id = target.id;
-        removeActiveClass();
+        disableTab();
         target.classList.add(activeClass);
-        const contentId = `${id}-content`;
-        const contentTarget = document.getElementById(contentId);
-        if (contentTarget) {
-            contentTarget.classList.add(activeClass);
-        }
+        target.dataset.tabActive = true;
+        enableProductContent(target.id);
     }
         
 }
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', addClass);
-});
+const enableProductContent = (id) => {
+    const contentId = `${id}-content`;
+    const contentTarget = document.getElementById(contentId);
+    if (contentTarget) {
+        contentTarget.classList.add(activeClass);
+    }
+}
 
-function toggle($event) {
+const toggleAccordion = ($event) => {
     $event.stopPropagation();
     const target = $event.currentTarget;
+
     if (target) {
-        
-        const activePanel = document.querySelector(`main .products .content .panel.${activeClass}`);
-        if (activePanel) {
-            activePanel.classList.toggle('active');
-        }
-
-        const activeAccordion = document.querySelector(`main .products .content .accordion.${activeClass}`);
-        if (activeAccordion) {
-            activeAccordion.classList.toggle('active');
-        }
-
-        const activePicture = document.querySelector(`main .products .content .product-photo img.${activeClass}`);
-        if (activePicture) {
-            activePicture.classList.toggle('active');
-        }
-
         target.classList.toggle('active');
-
-        const id = target.id;
-        const pictureId = `${id}-picture`;
-        const pictureItem = document.getElementById(pictureId);
-        pictureItem.classList.toggle('active');
-
-        const panel = target.nextElementSibling;
-        if (panel) {
-            panel.classList.toggle('active');
-        }
+        target.dataset.accordionActive = true;
+        
+        toggleAccordionPanel(target);
+        enableProductPicture(target.id);
     }
 }
 
-accordions.forEach(accordion => {
-    accordion.addEventListener('click', toggle);
-})
+const disableAccordion = () => {
+    const activeAccordion = document.querySelector('[data-accordion-active="true"]');
+    if (activeAccordion) {
+        activeAccordion.classList.toggle('active');
+        activeAccordion.dataset.accordionActive = false;
+    }
+}
+
+const toggleAccordionPanel = (target) => {
+    disableAccordionPanel();
+    const panel = target.nextElementSibling;
+    if (panel) {
+        panel.classList.toggle('active');
+        panel.dataset.accordionPanelActive = true;
+    }
+}
+
+const disableAccordionPanel = () => {
+    const activePanel = document.querySelector('[data-accordion-panel-active="true"]');
+    if (activePanel) {
+        activePanel.classList.toggle('active');
+        activePanel.dataset.accordionPanelActive = false;
+    }
+}
+
+const enableProductPicture = (id) => {
+    disablePicture();
+    const pictureId = `${id}-picture`;
+    const picture = document.getElementById(pictureId);
+    picture.classList.toggle('active');
+    picture.dataset.pictureActive = true;
+}
+
+const disablePicture = () => {
+    const activePicture = document.querySelector('[data-picture-active="true"]');
+    if (activePicture) {
+        activePicture.classList.toggle('active');
+        activePicture.dataset.pictureActive = false;
+    }
+}
+
+const initializeTabsBehavior = () => {
+    const tabs = document.querySelectorAll('[data-behavior="tabs"]');
+
+    tabs[0].classList.add(activeClass);
+    tabs[0].dataset.tabActive = true;
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', enableTab);
+    });
+}
+
+const initializeAccordionBehavior = () => {
+    const productContents = document.querySelectorAll('[data-behavior="tabs-content"]');
+    productContents[0].classList.add(activeClass);
+    
+    const accordions = productContents[0].querySelectorAll('[data-behavior="accordion"]');
+    
+    accordions.forEach(accordion => {
+        accordion.addEventListener('click', toggleAccordion);
+    })
+}
+
+initializeTabsBehavior();
+initializeAccordionBehavior();
